1b29cf7670ea8f5d6c59013f9e3b8125f1fb76b1
Irfani Silviana <ir.silviana@gmail.com>
2024-10-03 20:39:26 +0200

    improve page template

--- wp-content/themes/gymfitnesswptheme/page.php
+++ wp-content/themes/gymfitnesswptheme/page.php
@@ -1,19 +1,21 @@
 <?php get_header(); ?>
 
-<main class="container page">
+<main class="container page section no-sidebars">
 
   <?php while (have_posts()): the_post(); ?>
 
     <h1 class="text-center text-primary"><?php the_title(); ?></h1>
 
     <?php
-    if (has_post_thumbnail()): the_post_thumbnail('blog');
+    if (has_post_thumbnail()): the_post_thumbnail('blog', array(
+        'class' => 'featured-image'
+      ));
     endif;
     ?>
 
-    <div class="text-center">
-      <p> <?php the_content(); ?></p>
-    </div>
+
+    <p> <?php the_content(); ?></p>
+
 
   <?php endwhile; ?>
 </main>
