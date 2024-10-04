4c4b0949b040f9c6b9f304b64b4bbf0413f60ef0
Irfani Silviana <ir.silviana@gmail.com>
2024-10-04 14:39:05 +0200

    Displaying the result form WP_Query

--- wp-content/themes/gymfitnesswptheme/inc/queries.php
+++ wp-content/themes/gymfitnesswptheme/inc/queries.php
@@ -11,8 +11,21 @@ function gymfitness_classes_list()
     $classes = new WP_Query($args);
     while ($classes->have_posts()): $classes->the_post();
     ?>
+      <li class="gym-class card gradient">
+        <?php the_post_thumbnail('mediumSize'); ?>
+        <div class="content">
+          <a href="<?php the_permalink() ?>">
+            <h3> <?php the_title(); ?></h3>
+          </a>
 
-      <h3> <?php the_title(); ?></h3>
+          <p><?php the_field('class_days'); ?></p>
+          <?php
+          $start_time = get_field('start_time');
+          $end_time = get_field('end_time');
+          ?>
+          <p><?php echo $start_time . " to " . $end_time; ?></p>
+        </div>
+      </li>
 
     <?php endwhile;
     wp_reset_postdata(); ?>
