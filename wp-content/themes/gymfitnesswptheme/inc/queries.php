01ddda577f700cbc7dff39a5fb19122e231a9b19
Irfani Silviana <ir.silviana@gmail.com>
2024-10-04 19:54:54 +0200

    improve style on Classes page

--- wp-content/themes/gymfitnesswptheme/inc/queries.php
+++ wp-content/themes/gymfitnesswptheme/inc/queries.php
@@ -13,17 +13,17 @@ function gymfitness_classes_list()
     ?>
       <li class="gym-class card gradient">
         <?php the_post_thumbnail('mediumSize'); ?>
-        <div class="content">
+        <div class="card-content">
           <a href="<?php the_permalink() ?>">
             <h3> <?php the_title(); ?></h3>
           </a>
-
-          <p><?php the_field('class_days'); ?></p>
           <?php
           $start_time = get_field('start_time');
           $end_time = get_field('end_time');
           ?>
-          <p><?php echo $start_time . " to " . $end_time; ?></p>
+          <p><?php echo the_field('class_days') . " - " .
+                $start_time . " to " . $end_time ?></p>
+
         </div>
       </li>
 
