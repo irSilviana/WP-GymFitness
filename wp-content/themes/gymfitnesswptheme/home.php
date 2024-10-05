2850e7184e80dc861da4207fa0731f922367fa30
Irfani Silviana <ir.silviana@gmail.com>
2024-10-05 15:30:20 +0200

    Improve Blog page and single blog

--- wp-content/themes/gymfitnesswptheme/home.php
+++ wp-content/themes/gymfitnesswptheme/home.php
@@ -1,12 +1,32 @@
 <?php get_header(); ?>
 
 <main class="container page section">
-  <?php while (have_posts()): the_post(); ?>
+  <ul class="blog-entries">
+    <?php while (have_posts()): the_post(); ?>
+      <li class="card gradient">
+        <?php the_post_thumbnail('mediumSize'); ?>
+        <div class="card-content">
+          <a href="<?php the_permalink() ?>">
+            <h3>
+              <?php the_title(); ?>
+            </h3>
+          </a>
+          <p class="meta"><span>By: </span>
+            <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')) ?>">
+              <?php echo get_the_author_meta('display_name') ?>
+            </a>
+          </p>
+          <p class="date-published meta text-primary">
+            Date:
+            <span class="text-white">
+              <?php the_time(get_option('date_format')) ?>
+            </span>
+          </p>
+        </div>
 
-    <h2>
-      <?php the_title() ?>
-    </h2>
-  <?php endwhile; ?>
+      </li>
+    <?php endwhile; ?>
+  </ul>
 </main>
 
 <?php get_footer(); ?>
