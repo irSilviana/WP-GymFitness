339e0d0f3463119c1102be74441833f205a04996
Irfani Silviana <ir.silviana@gmail.com>
2024-10-05 20:30:16 +0200

    refactor Blog & Author page and improve style

--- wp-content/themes/gymfitnesswptheme/home.php
+++ wp-content/themes/gymfitnesswptheme/home.php
@@ -1,32 +1,7 @@
 <?php get_header(); ?>
 
 <main class="container page section">
-  <ul class="blog-entries">
-    <?php while (have_posts()): the_post(); ?>
-      <li class="card gradient">
-        <?php the_post_thumbnail('mediumSize'); ?>
-        <div class="card-content">
-          <a href="<?php the_permalink() ?>">
-            <h3>
-              <?php the_title(); ?>
-            </h3>
-          </a>
-          <p class="meta"><span>By: </span>
-            <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')) ?>">
-              <?php echo get_the_author_meta('display_name') ?>
-            </a>
-          </p>
-          <p class="date-published meta text-primary">
-            Date:
-            <span class="text-white">
-              <?php the_time(get_option('date_format')) ?>
-            </span>
-          </p>
-        </div>
-
-      </li>
-    <?php endwhile; ?>
-  </ul>
+  <?php get_template_part('template-parts/blog', 'loop') ?>
 </main>
 
 <?php get_footer(); ?>
