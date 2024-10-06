5a4801485bbd82d0e4b7032403b9e55caeb54489
Irfani Silviana <ir.silviana@gmail.com>
2024-10-06 14:52:10 +0200

    Add Welcome and area section to the Front-page

--- wp-content/themes/gymfitnesswptheme/front-page.php
+++ wp-content/themes/gymfitnesswptheme/front-page.php
@@ -1,4 +1,40 @@
 <?php get_header('front') ?>
 
+<?php while (have_posts()): the_post(); ?>
+  <section class="welcome text-center section">
+    <h2 class="text-primary">
+      <?php the_field('welcome_heading') ?>
+    </h2>
+    <p>
+      <?php the_field('welcome_text') ?>
+    </p>
+  </section>
+
+  <section class="section-areas">
+    <ul class="areas-container">
+      <?php
+      $areas = array(
+        0 => get_field('area_1'),
+        1 => get_field('area_2'),
+        2 => get_field('area_3'),
+        3 => get_field('area_4'),
+      );
+
+      foreach ($areas as $key => $value) {
+      ?>
+        <li class="area">
+          <?php
+          $key = get_field('area_' . $key + 1);
+          $image = wp_get_attachment_image_src($key['area_image'], 'mediumSize')[0];
+          ?>
+          <img src="<?php echo $image ?>">
+          <p><?php echo $key['area_name']; ?></p>
+        </li>
+      <?php
+      }
+      ?>
+    </ul>
+  </section>
+<?php endwhile; ?>
 
 <?php get_footer() ?>
