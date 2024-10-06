1a92c087fb43f42852a0385778961b3a24699b71
Irfani Silviana <ir.silviana@gmail.com>
2024-10-06 20:09:09 +0200

    display Blog on the front-page and refactor code

--- wp-content/themes/gymfitnesswptheme/author.php
+++ wp-content/themes/gymfitnesswptheme/author.php
@@ -6,12 +6,16 @@
   ?>
 
   <h2 class="text-center primary-text">
-    Author: &nbsp; <?php echo $author->data->display_name; ?>
+    Author: <?php echo $author->data->display_name; ?>
   </h2>
   <p class="text-center">
     <?php echo get_the_author_meta('description', $author->data->ID) ?>
   </p>
-  <?php get_template_part('template-parts/blog', 'loop') ?>
+  <ul class="blog-entries">
+    <?php while (have_posts()): the_post(); ?>
+      <?php get_template_part('template-parts/blog', 'loop') ?>
+    <?php endwhile; ?>
+  </ul>
 </main>
 
 <?php get_footer(); ?>
--- wp-content/themes/gymfitnesswptheme/category.php
+++ wp-content/themes/gymfitnesswptheme/category.php
@@ -3,14 +3,18 @@
 <main class="container page section">
   <?php
   $category = get_queried_object(); ?>
-  <h2 class="text-center text-primary">Category: &nbsp;<?php echo $category->name; ?>
+  <h2 class="text-center text-primary">Category: <?php echo $category->name; ?>
   </h2>
   <div class="text-center">
     <p>
       <?php echo category_description($category->ID) ?>
     </p>
   </div>
-  <?php get_template_part('template-parts/blog', 'loop') ?>
+  <ul class="blog-entries">
+    <?php while (have_posts()): the_post(); ?>
+      <?php get_template_part('template-parts/blog', 'loop') ?>
+    <?php endwhile; ?>
+  </ul>
 </main>
 
 <?php get_footer(); ?>
